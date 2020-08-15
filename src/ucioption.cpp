83fde9f8de06380a0f19e9760e237411868fbaaf
massimilianogoi <33367597+massimilianogoi@users.noreply.github.com>
2020-08-16 01:44:28 +0200

    Matching Stockfish Beta having Timestamp: 1597415992 - Try to match relative magnitude of NNUE eval to classical.
    Setting to use the Sergio's best network (20200720-1017.bin) as default. Notice that you will need this network to compile
    Stockfish Polyglot but you won't find it in this GitHub repository because I'm not sure it's possible to reshare it.
    You can download it at this address: https://www.comp.nus.edu.sg/~sergio-v/nnue/20200720-1017.bin .
    
    Modifying the build.bat file to write the proper data suffix in the file name (DDMMYY).

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -1,8 +1,6 @@
 /*
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
-  Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
-  Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
-  Copyright (C) 2015-2020 Marco Costalba, Joona Kiiski, Gary Linscott, Tord Romstad
+  Copyright (C) 2004-2020 The Stockfish developers (see AUTHORS file)
 
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -50,7 +48,8 @@ void on_book_file3(const Option& o) { polybook3.init(o); }
 void on_book_file4(const Option& o) { polybook4.init(o); }
 void on_best_book_move(const Option& o) { polybook.set_best_book_move(o); }
 void on_book_depth(const Option& o) { polybook.set_book_depth(o); }
-
+void on_use_NNUE(const Option& ) { Eval::init_NNUE(); }
+void on_eval_file(const Option& ) { Eval::init_NNUE(); }
 
 /// Our case insensitive less() function as required by UCI protocol
 bool CaseInsensitiveLess::operator() (const string& s1, const string& s2) const {
@@ -97,6 +96,10 @@ void init(OptionsMap& o) {
   o["BookFile 4"]            << Option("", on_book_file4);
   o["BestBookMove"]          << Option(false, on_best_book_move); /// having this function disabled avoids repetitions in books testing
   o["BookDepth"]             << Option(255, 1, 255, on_book_depth);
+  o["Use NNUE"]              << Option(true, on_use_NNUE);
+  // The default must follow the format nn-[SHA256 first 12 digits].nnue
+  // for the build process (profile-build and fishtest) to work.
+  o["EvalFile"]              << Option("20200720-1017.bin", on_eval_file);
 }
 
 
